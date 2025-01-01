afddff38ad761ad1431c288dbef29e05f3d70120
Sidhant-Jha <siddhantjhacs@gmail.com>
2025-01-02 00:22:30 +0530

    delete main folders from bloc branch

--- Cart_Application/android/app/src/main/kotlin/com/example/cart_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cart_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Pixel_Art_App/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:pixel_art/application.dart';
-
-void main()
-{
-  runApp(Application());
-}
